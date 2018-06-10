dfc778666bf820104f9ed5796f7513ca96260527
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2018-06-10 15:27:43 +0200

    Added Typescript support, updated package.json, added TS Lint, nodemon for server restarts, and bunch of other stuff

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Lazy Language to the rescue for us lazy monkeys trying to learn a new language')
-})
-
-app.listen(3000)
