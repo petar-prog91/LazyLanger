986ba59a4a7148aa8dd3c5c98d543b57e947ccb3
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2018-06-10 15:34:26 +0200

    Extended tslint with microsoft rules and fixed indentation

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,28 @@
 import * as express from 'express'
 
 class App {
-  public express
+    public express
 
-  constructor () {
-    this.express = express()
-    this.mountRoutes()
-  }
+    constructor() {
+        this.express = express()
+        this.mountRoutes()
+    }
 
-  private mountRoutes (): void {
-    const router = express.Router()
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Lazy Language to the rescue for us lazy monkeys trying to learn a new language!'
-      })
-    })
-    this.express.use('/', router)
-  }
+    private mountRoutes(): void {
+        const router = express.Router()
+        router.get('/', (req, res) => {
+            res.json({
+                message: 'Lazy Language to the rescue for us lazy monkeys trying to learn a new language!'
+            })
+        })
+
+        router.get('/', (req, res) => {
+            res.json({
+                message: 'Lazy Language to the rescue for us lazy monkeys trying to learn a new language!'
+            })
+        })
+        this.express.use('/', router)
+    }
 }
 
 export default new App().express
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import app from './App'
 const port = process.env.PORT || 3000
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
+    if (err) {
+        return console.log(err)
+    }
 
-  return console.log(`server is listening on ${port}`)
+    return console.log(`server is listening on ${port}`)
 })
